445e1f70bf6a300f19b13137917b08b5d257790a
Abdulcabbar Sezgin <95128154+Sezgincabbar@users.noreply.github.com>
2022-08-13 17:23:53 +0300

    build

--- pages/_document.js
+++ pages/_document.js
@@ -86,7 +86,12 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script async src="jquery-3.5.1.js"></script>
+          <script
+            async
+            src="https://code.jquery.com/jquery-3.6.0.js"
+            integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+            crossorigin="anonymous"
+          ></script>
           {/* <!-- jQuery --> */}
           <script async type="text/javascript" src="js/jquery.min.js"></script>
 
